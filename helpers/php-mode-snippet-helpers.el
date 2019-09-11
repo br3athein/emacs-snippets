c11c3be2ba893ffc66059a7fbfc524f3d2b41127
br3athein <br3athein@gmail.com>
2019-09-11 11:29:03 +0300

    php/namespace: Sanitize namespace snippet
    There's a package named phpactor out there, which seems cool at the
    first sight. And, yes, it can guess namespaces, too.

--- helpers/php-mode-snippet-helpers.el
+++ helpers/php-mode-snippet-helpers.el
@@ -23,14 +23,6 @@ For instance, given the signature \"Foo $bar, $baz\", return
                       (substring it (if (s-starts-with? "$" it) 1))))
                it)))
 
-(defun yas-php-guess-namespace (&optional namespace-root)
-  "Guess the current class namespace."
-  (--> (spacemacs--projectile-directory-path)
-       (directory-file-name it)
-       (string-remove-prefix (concat namespace-root "/") it)
-       (subst-char-in-string ?/ ?\\ it)
-       (upcase-initials it)))
-
 ;;; _
 (provide 'php-mode-snippet-helpers)
 ;;; php-mode-snippet-helpers.el ends here
